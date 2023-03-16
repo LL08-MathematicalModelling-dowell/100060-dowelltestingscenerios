d81b8623b8b9eed0bd6a3d4cc9282290a6dcad1f
ahmednagra <ahmednagra9@gmail.com>
2023-03-16 20:38:16 +0500

    adminui/ux for youtube & file_app Modules

--- testrecorder/file_app/admin.py
+++ testrecorder/file_app/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 
 from file_app.models import TestRecords, MegaTestRecord, VpsTestRecord
 
+"""
 admin.site.register(TestRecords)
 admin.site.register(MegaTestRecord)
 admin.site.register(VpsTestRecord)
+"""
+
+@admin.register(TestRecords)
+class TestRecordsAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'test_description',
+                    'test_name', 'webcam_file', 'merged_webcam_screen_file','timestamp')
+
+
+@admin.register(MegaTestRecord)
+class MegaTestRecordAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'test_description',
+                    'test_name', 'webcam_file', 'merged_webcam_screen_file','beanote_file','timestamp')
+
+
+@admin.register(VpsTestRecord)
+class VpsTestRecordAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'test_description',
+                    'test_name', 'user_files_timestamp','webcam_file', 'merged_webcam_screen_file','beanote_file','timestamp')
--- testrecorder/youtube/admin.py
+++ testrecorder/youtube/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
-from youtube.models import UserYoutubePlaylists,ChannelsRecord
+from youtube.models import UserYoutubePlaylists,ChannelsRecord, PlaylistsRecord
 
+"""
 admin.site.register(UserYoutubePlaylists)
 admin.site.register(ChannelsRecord)
+
+"""
+
+""" Muhammad Ahmed  """
+@admin.register(UserYoutubePlaylists)
+class UserYoutubePlaylistsAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'playlist_id',
+                    'playlist_title', 'playlist_enabled')
+
+
+@admin.register(PlaylistsRecord)
+class PlaylistsRecordAdmin(admin.ModelAdmin):
+    list_display = ('playlist_id', 'playlist_title', 'timestamp')
+
+
+@admin.register(ChannelsRecord)
+class ChannelsRecordAdmin(admin.ModelAdmin):
+    list_display = ('channel_id', 'channel_title',
+                    'channel_credentials', 'timestamp')
