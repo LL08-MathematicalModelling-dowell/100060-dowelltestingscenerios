ecb4226ff3a61eacbfb1e1d21f9f0b3c44840b3d
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-05-01 23:31:59 +0100

    New version 5/1/2023

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['100034.pythonanywhere.com']
 ALLOWED_HOSTS = ['*']
@@ -112,7 +112,7 @@ SOCIALACCOUNT_PROVIDERS = {
         'OAUTH_PKCE_ENABLED': True,
     }
 }
-# ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
 
 
 WSGI_APPLICATION = 'testrecorder.wsgi.application'
@@ -127,29 +127,29 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': os.getenv("DATABASE_NAME"),
-#         'ENFORCE_SCHEMA': False,
-#         'CLIENT': {
-#             'host': os.getenv("DATABASE_HOST")
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': os.getenv("DATABASE_NAME"),
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': os.getenv("DATABASE_HOST")
+        }
+    }
+}
 
 
 '''
 This block of code was added to fixed a bug/shortfall in the Djongo liberary
-This resulted in errors during google login through allauth
-Djongo can't the "booblean condition" and expects "table"."bool_field" = True.
+That resulted in errors during google login through allauth
+Djongo can't handle the "booblean condition" and expects "table"."bool_field" = True.
 Django has an option for enabling these "boolean conditions".
 By overriding  the config by adding this code snipet to djongo/operations.py,
 it is compared against TRUE in the SQL and Djongo can handle it.
