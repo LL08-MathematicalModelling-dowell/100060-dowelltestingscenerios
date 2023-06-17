b841cb764a2e2c14ee328edf7984657d56ece47e
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-06-17 23:22:03 +0100

    Before merging with add_channel

--- testrecorder/home/urls.py
+++ testrecorder/home/urls.py
@@ -4,7 +4,6 @@ from .views import HomePageView, CalendlyPageView, AboutPageView, records_view, 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    # path('privacy/', privacy_page, name='privacy_page'),
     path('recordings/', records_view, name='view_records'),
     path('calendly/', CalendlyPageView.as_view(), name='calendly'),
     path('about/', AboutPageView.as_view(), name='about'),
