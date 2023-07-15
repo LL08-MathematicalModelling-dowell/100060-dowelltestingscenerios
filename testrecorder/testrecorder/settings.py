05e71e8b761ecc63b8ce81b7f9d82dd93cf26bb5
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-07-15 20:33:22 +0100

    New version 7/15/2023; This version includes working camera, screan and screen+camera recording, it is also design with Gsome gogle YouTube specifiations implemented

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -148,11 +148,11 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 # DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
