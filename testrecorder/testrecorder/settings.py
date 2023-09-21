a4811dc24cf4924b1cf1efe118ffb1379e15cfe3
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-09-21 15:21:16 +0100

    Implemented caching on chanel and playlist fetch

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -219,10 +219,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # STATIC_ROOT = ''
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 # Temporary files directory
