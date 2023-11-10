8a22190b062b4b5bf0343f8af0fc05963e489811
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-11-10 07:24:38 +0100

    Implemented deployment settings

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -221,7 +221,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATIC_ROOT = os.patsh.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
