1a1b8b42cde83b0b8a24cd3ec9a7c167ebae2c82
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-11-13 20:00:54 +0100

    New version 13.11.2023.19:39 with bug fixes

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -215,7 +215,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
-'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = '/static/'
