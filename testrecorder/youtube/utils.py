35ac74284e01e43cb0ee172090d7461fbf99d7ee
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-11-15 09:56:36 +0100

    Implemented caching for user yutube object

--- testrecorder/youtube/utils.py
+++ testrecorder/youtube/utils.py
@@ -1,4 +1,5 @@
 import json
+from django.core.cache import cache
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from .models import YoutubeUserCredential
@@ -17,6 +18,14 @@ def create_user_youtube_object(request):
     """
     print('Creating youtube object...')
     try:
+        try:
+            cache_key = get_user_cache_key(request.user.id, 'youtube_credenial_object')
+            youtube, credentials = cache.get(cache_key)
+            if youtube and credentials:
+                return youtube, credentials
+        except Exception:
+            pass
+
         # Retrieve the YoutubeUserCredential object associated with the authenticated user
         youtube_user = YoutubeUserCredential.objects.get(user=request.user)
 
@@ -44,15 +53,19 @@ def create_user_youtube_object(request):
                 # Update the stored credential data with the refreshed token
                 youtube_user.credential = credentials.to_json()
                 youtube_user.save()
-                print('Access token refreshed!')
+                # print('Access token refreshed!')
         except Exception as e:
             # Handle any error that occurred while refreshing the access token
-            print(f'An error occurred: {e}')
-            return None
+            # print(f'An error occurred: {e}')
+            return None, None
 
         # Create a YouTube object using the v3 version of the API and the retrieved credentials
         youtube = build('youtube', 'v3', credentials=credentials, cache_discovery=False)
         
+        if cache_key:
+            # Cache the youtube object
+            cache.set(cache_key, (youtube, credentials), 86400)
+
         return youtube, credentials
     except YoutubeUserCredential.DoesNotExist:
-        return None
+        return None, None
