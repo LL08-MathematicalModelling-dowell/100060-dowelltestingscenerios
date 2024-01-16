42acceba189b4df1261c645d6947611679fdd28e
Goldneo10 <ibrahimmuhammad271@gmail.com>
2024-01-17 00:00:49 +0100

    Fixed daphne deploment error

--- testrecorder/testrecorder/celery.py
+++ testrecorder/testrecorder/celery.py
@@ -1,11 +1,11 @@
-import os
-from celery import Celery
+# import os
+# from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testrecorder.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testrecorder.settings")
 
-celery = Celery("testrecorder")
+# celery = Celery("testrecorder")
 
-celery.config_from_object("django.conf:settings", namespace="CELERY")
+# celery.config_from_object("django.conf:settings", namespace="CELERY")
 
-celery.autodiscover_tasks()
+# celery.autodiscover_tasks()
