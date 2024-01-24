dfbdcf976b539cd7d8a6cf08a76c09cf4f97c866
Goldneo10 <ibrahimmuhammad271@gmail.com>
2024-01-24 14:20:27 +0100

    New version 24/01/2024
    Whats new?
    The cut video stream is fixed in this version.

--- testrecorder/app_websocket/consumers.py
+++ testrecorder/app_websocket/consumers.py
@@ -1,17 +1,16 @@
+from youtube.models import UserProfile
+from youtube.utils import transition_broadcast
 import os
 import subprocess
 import django
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
 from django.core.cache import cache
+import time
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testrecorder.settings')
 django.setup()
-
-
-from youtube.utils import transition_broadcast
-from youtube.models import UserProfile
 
 
 @database_sync_to_async
@@ -25,6 +24,7 @@ def get_user(api_key):
 
 class VideoConsumer(AsyncConsumer):
     """Socket Consumer that accept websocket connection and live stream"""
+
     def __init__(self):
         self.process_manager = FFmpegProcessManager(send=self.send)
 
@@ -39,13 +39,13 @@ class VideoConsumer(AsyncConsumer):
                     self.scope['user'] = user
                     await self.send({"type": "websocket.accept"})
                 else:
-                    await self.send({"type": "websocket.close", "text": "UnAuthorised" })
+                    await self.send({"type": "websocket.close", "text": "UnAuthorised"})
                     await self.websocket_disconnect(event)
             else:
-                await self.send({"type": "websocket.close", "text": "UnAuthorised" })
+                await self.send({"type": "websocket.close", "text": "UnAuthorised"})
                 await self.websocket_disconnect(event)
         except UserProfile.DoesNotExist:
-            await self.send({"type": "websocket.close", "text": "UnAuthorised" })
+            await self.send({"type": "websocket.close", "text": "UnAuthorised"})
             await self.websocket_disconnect(event)
 
     async def websocket_receive(self, event):
@@ -73,9 +73,9 @@ class VideoConsumer(AsyncConsumer):
     async def process_command_event(self, command):
         """Process the command event"""
         if command == 'end_broadcast':
-            success = self.process_manager.end_broadcast(self.scope)
+            success = self.process_manager.process_manager_cleanup(self.scope)
+            # success = self.process_manager.end_broadcast(self.scope)
             await self.send({"type": "websocket.send", "text": "Success" if success else "Failed"})
-            self.process_manager.process_manager_cleanup()
 
     async def process_bytes_event(self, bytes_data):
         """Process the bytes event"""
@@ -88,6 +88,7 @@ class VideoConsumer(AsyncConsumer):
 
 class FFmpegProcessManager:
     """ Manages the FFmpeg process """
+
     def __init__(self, send=None):
         self.process = None
         self.rtmp_url = None
@@ -100,7 +101,7 @@ class FFmpegProcessManager:
         self.start_ffmpeg_process()
 
         return self.rtmp_url
-       
+
     def handle_rtmp_url(self, data):
         """Handle the rtmp url message"""
         self.audio_enabled = False
@@ -108,10 +109,10 @@ class FFmpegProcessManager:
         self.start_ffmpeg_process()
 
         return self.rtmp_url
-        
+
     def end_broadcast(self, scope):
         """Ends the broadcast"""
-        success = self.transition_broadcast(scope=scope)        
+        success = self.transition_broadcast(scope=scope)
         return success
 
     def handle_bytes_data(self, bytes_data):
@@ -123,16 +124,17 @@ class FFmpegProcessManager:
         """Cleanup when the websocket disconnects"""
         if self.process:
 
-            self.process_manager_cleanup()
-            _ = self.transition_broadcast(scope)
-            cache.delete(f"stream_dict{scope.get('user').id}")
+            self.process_manager_cleanup(scope)
+            # _ = self.transition_broadcast(scope)
+            # cache.delete(f"stream_dict{scope.get('user').id}")
 
     def transition_broadcast(self, scope):
         """Transition the broadcast"""
         success = False
         if self.process:
             try:
-                stream_dict = cache.get(f"stream_dict{scope.get('user').id}", None)
+                stream_dict = cache.get(
+                    f"stream_dict{scope.get('user').id}", None)
                 if stream_dict:
                     broadcast_status = 'complete'
                     trans_dict = transition_broadcast(
@@ -150,14 +152,14 @@ class FFmpegProcessManager:
 
         return success
 
-    def process_manager_cleanup(self):
+    def process_manager_cleanup(self, scope):
         """Cleanup the process manager"""
         try:
             if self.process:
                 if not self.process.stdin.closed:
                     self.process.stdin.close()
 
-                _, stderr = self.process.communicate()
+                _, stderr = self.process.communicate(timeout=35)
 
                 if stderr:
                     print("Error in subprocess stderr:", stderr)
@@ -165,15 +167,18 @@ class FFmpegProcessManager:
                 self.process.wait()
         except subprocess.TimeoutExpired:
             self.process.terminate()
-    
+
         except Exception as e:
             print("Error while closing the subprocess: ", e)
 
         finally:
             self.process = None
+            success = self.transition_broadcast(scope)
+            cache.delete(f"stream_dict{scope.get('user').id}")
+
+            return success
 
     def start_ffmpeg_process(self):
-        """Start the ffmpeg process"""
         try:
             command = self.generate_ffmpeg_command()
             self.process = subprocess.Popen(
@@ -181,34 +186,36 @@ class FFmpegProcessManager:
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
-            
         except Exception as e:
             print("Error starting FFmpeg process: ", e)
 
     def generate_ffmpeg_command(self):
-        """Generate the ffmpeg command"""
-        command = [
+        # common_options = [
+        #     'ffmpeg',
+        #     '-vcodec', 'copy',
+        #     '-acodec', 'aac',
+        #     '-f', 'flv',
+        #     '-preset', 'ultrafast',
+        #     self.rtmp_url,
+        # ]
+        common_options = [
             'ffmpeg',
-            '-i', '-',
             '-vcodec', 'copy',
             '-acodec', 'aac',
             '-f', 'flv',
-            self.rtmp_url
+            '-preset', 'ultrafast',
+            '-tune', 'zerolatency',  # Enable zerolatency tuning
+            self.rtmp_url,
         ]
 
         if not self.audio_enabled:
-            command = [
-                'ffmpeg',
+            return common_options + [
                 '-f', 'lavfi', '-i', 'anullsrc',
                 '-i', '-',
                 '-shortest',
-                '-vcodec', 'copy',
-                '-acodec', 'aac',
-                '-f', 'flv',
-                self.rtmp_url
             ]
 
-        return command
+        return common_options + ['-i', '-']
 
     def extract_rtmp_url(self, data):
         """Extract the rtmp url from the data"""
--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -147,6 +147,7 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
