73427638c5690b00368572cab37f269dbd0aea12
Jordano Bruno <mohammad.dehghan218@gmail.com>
2024-03-01 14:24:16 +0100

    Update settings.py
    ..

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your_default_secret_key_for_development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
