5c20c5f33f0eb8f5ce3f1066d013a149370ed5b6
Jordano Bruno <mohammad.dehghan218@gmail.com>
2024-03-01 14:08:42 +0100

    error 500

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your_default_secret_key_for_development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
