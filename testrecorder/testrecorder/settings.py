63bcf03a851a47d451126368075b738121526d28
Jordano Bruno <mohammad.dehghan218@gmail.com>
2024-03-02 15:22:12 +0100

    there are some problem with static files loading

--- testrecorder/testrecorder/settings.py
+++ testrecorder/testrecorder/settings.py
@@ -224,10 +224,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Temporary files directory
 TEMP_FILES_ROOT = os.path.join(BASE_DIR, "temp")
