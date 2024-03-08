90e98036a37826c984b49715bada20826d0b993d
Jamin Onuegbu <76646536+JaminCO@users.noreply.github.com>
2024-03-08 09:37:46 -0800

    Update index.js
    Commented out logs that where used for testing

--- testrecorder/static/js/index.js
+++ testrecorder/static/js/index.js
@@ -777,11 +777,11 @@ async function startRecording() {
              if (navigator.onLine){
                 if (socket.readyState === WebSocket.OPEN) {
                   // Send buffered data if there's internet connection and the socket is open
-                  console.log("Sending....")
+                  // console.log("Sending....")
                   sendDataBuffer();
               }
              } else {
-              console.log("Thou art Offline")
+              // console.log("Thou art Offline")
              }
           }
        };
@@ -792,7 +792,7 @@ async function startRecording() {
       
           if (navigator.onLine && socket.readyState === WebSocket.OPEN) {
             // Send any remaining buffered data
-            console.log("Send any remaining buffered data")
+            // console.log("Send any remaining buffered data")
             sendDataBuffer();
           }
         };
@@ -818,8 +818,8 @@ async function startRecording() {
   async function sendDataBuffer() {
     while (mediaBuffer.length > 0) {
        const data = mediaBuffer.shift(); // Get and remove the first item from the buffer
-       console.log("Sending data to socket, data: ")
-       console.log(data)
+       // console.log("Sending data to socket, data: ")
+       // console.log(data)
        await socket.send(data);
     }
  }
